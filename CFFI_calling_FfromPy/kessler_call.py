400e72f49144975971493252860740424ea3f42c
djarecka <djarecka@gmail.com>
2015-02-10 21:38:06 -0700

    changing nx and nz to be bigger than 1; had to change order numpy arrays to Fortran

--- CFFI_calling_FfromPy/kessler_call.py
+++ CFFI_calling_FfromPy/kessler_call.py
@@ -5,26 +5,33 @@ sys.path.append(".")
 import numpy as np
 from cffi_kessler import kessler
 
-nx = 1
+nx = 2
 ny = 1
-nz = 1
+nz = 3
 dt_in = 1.
 
 variable_nparr = {}
-variable_nparr["t"] = np.ones((nx,nz,ny)) * 291.8
-variable_nparr["qv"] = np.ones((nx,nz,ny)) * 10.e-3
-variable_nparr["qc"] = np.ones((nx,nz,ny)) * 0.e-3
-variable_nparr["rho"] = np.ones((nx,nz,ny))
-variable_nparr["pii"] = np.ones((nx,nz,ny)) * .97
-variable_nparr["dz8w"] = np.ones((nx,nz,ny)) * 20.
-variable_nparr["z"] = np.ones((nx,nz,ny)) * 700.
-variable_nparr["qr"] = np.zeros((nx,nz,ny))
+variable_nparr["t"] = np.asfortranarray(np.ones((nx,nz,ny))) * 291.8
+variable_nparr["qv"] = np.asfortranarray(np.ones((nx,nz,ny))) * 10.e-3
+variable_nparr["qv"][:,0,:] *= 0.9
+variable_nparr["qv"][:,2,:] *= 1.1
+variable_nparr["qc"] = np.ones((nx,nz,ny), order='F') * 0.e-3
+variable_nparr["rho"] = np.ones((nx,nz,ny), order='F')
+variable_nparr["pii"] = np.ones((nx,nz,ny), order='F') * .97
+variable_nparr["dz8w"] = np.ones((nx,nz,ny), order='F') * 20.
+variable_nparr["z"] = np.ones((nx,nz,ny), order='F') * 700.
+variable_nparr["qr"] = np.zeros((nx,nz,ny), order='F')
 for var_nm in ["RAINNC", "RAINNCV"]:
-    variable_nparr[var_nm] = np.zeros((nx,ny))
+    variable_nparr[var_nm] = np.zeros((nx,ny), order='F')
 
-print "before calling the microphysical scheme: temperature, T = %2.2f; water vapour mixing ratio, qv =  %2.4f; and cloud water mixing ratio, qc =  %2.4f" % (variable_nparr["t"], variable_nparr["qv"], variable_nparr["qc"])
+#print "before calling the microphysical scheme: temperature, T = %2.2f; water vapour mixing ratio, qv =  %2.4f; and cloud water mixing ratio, qc =  %2.4f" % (variable_nparr["t"][0], variable_nparr["qv"][0], variable_nparr["qc"][0])
+
+np.set_printoptions(precision=4)
+print "water vapour before microphysics \n",  variable_nparr["qv"][:,:,0], "\n", "liquid water  before microphysics \n", variable_nparr["qc"][:,:,0], "\n"
 
 kessler(nx, ny, nz, dt_in, variable_nparr)
 
-print "after calling the microphysical scheme: temperature, T = %2.2f; water vapour mixing ratio, qv =  %2.4f; and cloud water mixing ratio, qc =  %2.4f" % (variable_nparr["t"], variable_nparr["qv"], variable_nparr["qc"])
+#print "after calling the microphysical scheme: temperature, T = %2.2f; water vapour mixing ratio, qv =  %2.4f; and cloud water mixing ratio, qc =  %2.4f" % (variable_nparr["t"], variable_nparr["qv"], variable_nparr["qc"])
+
+print "water vapour after microphysics \n",  variable_nparr["qv"][:,:,0], "\n", "liquid water after microphysics \n", variable_nparr["qc"][:,:,0], "\n"
 
